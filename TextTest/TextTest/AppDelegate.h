afde2747b5258781fcec7cdba382e4caedbdd359
renxinwei <362922604@qq.com>
2022-11-25 09:56:55 +0800

    添加测试demo

---
+++ TextTest/TextTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TextTest
+//
+//  Created by renxinwei on 2022/11/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TextTest/TextTest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TextTest
+//
+//  Created by renxinwei on 2022/11/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
