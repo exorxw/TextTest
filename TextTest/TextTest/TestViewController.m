3e0b734f151632666622c97c30b73871f93bda79
renxinwei <362922604@qq.com>
2022-11-27 20:39:38 +0800

    更新测试

--- TextTest/TextTest/TestViewController.m
+++ TextTest/TextTest/TestViewController.m
@@ -27,13 +27,14 @@
     // 创建粒子Layer
     CAEmitterLayer *snowEmitter = [CAEmitterLayer layer];
     // 粒子发射位置
-    snowEmitter.emitterPosition = CGPointMake(self.view.width/2, -10);
+    snowEmitter.emitterPosition = CGPointMake(self.view.width/2, -20);
     // 发射源的尺寸大小
     snowEmitter.emitterSize = self.view.bounds.size;
     // 发射模式
     snowEmitter.emitterMode = kCAEmitterLayerSurface;
     // 发射源的形状
     snowEmitter.emitterShape = kCAEmitterLayerLine;
+    
     // 创建雪花类型的粒子
     CAEmitterCell *snowflake = [CAEmitterCell emitterCell];
     // 粒子的名字
@@ -54,6 +55,28 @@
     snowflake.contents = (id)[[UIImage imageNamed:@"img_iap_snow"] CGImage];
     // 设置雪花形状的粒子的颜色
     snowflake.color = [[UIColor whiteColor] CGColor];
+    
+    // 创建雪花类型的粒子
+    CAEmitterCell *snowflake2 = [CAEmitterCell emitterCell];
+    // 粒子的名字
+    snowflake2.name = @"iapsnow";
+    // 粒子参数的速度乘数因子
+    snowflake2.birthRate = 15.0;  //每秒生成数量
+    snowflake2.lifetime = 28;        //生存时间
+    // 粒子速度
+    snowflake2.velocity = 9;
+    // 粒子的速度范围
+    snowflake2.velocityRange = 10;
+    // 粒子y方向的加速度分量
+    snowflake2.yAcceleration = 8;
+    // 周围发射角度
+    snowflake2.emissionRange = 0.5 * M_PI;
+    // 子旋转角度范围
+    snowflake2.spinRange = 0.25 * M_PI;
+    snowflake2.contents = (id)[[UIImage imageNamed:@"btn_home_gift_temp"] CGImage];
+    // 设置雪花形状的粒子的颜色
+    snowflake2.color = [[UIColor whiteColor] CGColor];
+    
     //缩放范围
     snowflake.scaleRange = 0.5f;
     snowflake.scale = 0.05f;
@@ -63,13 +86,20 @@
     // 粒子边缘的颜色
     snowEmitter.shadowColor = [[UIColor whiteColor] CGColor];
     // 添加粒子
-    snowEmitter.emitterCells = @[snowflake];
+    snowEmitter.emitterCells = @[snowflake, snowflake2];
     // 将粒子Layer添加进图层中
     [self.view.layer addSublayer:snowEmitter];
 }
 
 - (void)setupViews {
     self.view.backgroundColor = UIColor.blackColor;
+    
+    UIImageView *bgImageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
+    bgImageView.image = [UIImage imageNamed:@"bg_iap_temp"];
+    bgImageView.contentMode = UIViewContentModeScaleAspectFill;
+    [self.view addSubview:bgImageView];
+    
+    
     UILabel *label = [UILabel new];
     label.frame = CGRectMake(100, 100, 100, 100);
     label.text = @"==测试==";
