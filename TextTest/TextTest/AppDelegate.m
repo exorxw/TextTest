7981dab37fe7194de31119db43b095861414fcc5
renxinwei <362922604@qq.com>
2022-12-21 17:14:55 +0800

    添加字体示例

--- TextTest/TextTest/AppDelegate.m
+++ TextTest/TextTest/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 #import "TestViewController.h"
 
 @interface AppDelegate ()
@@ -18,8 +19,16 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.rootViewController = [TestViewController new];
+    self.window.rootViewController = [ViewController new];
     [self.window makeKeyAndVisible];
+    
+    NSLog(@"app language:%@", [NSLocale preferredLanguages]);
+    for (NSString *fontFamilyName in [UIFont familyNames]) {
+        NSLog(@"--- %@ ---", fontFamilyName);
+        for (NSString *fontName in [UIFont fontNamesForFamilyName:fontFamilyName]) {
+            NSLog(@"%@", fontName);
+        }
+    }
     
     return YES;
 }
